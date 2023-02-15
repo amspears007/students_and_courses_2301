56bb44e1dc12bce556b9d4a779506ad97b23c50e
Amy Spears <amspears007@gmail.com>
2023-02-15 09:14:23 -0700

    Add student attributes test

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Student do
 
     expect(student).to be_a(Student) 
   end
+
+  it 'has a name and age' do
+    student = Student.new({name: "Morgan", age: 21}) 
+
+    expect(student.name).to eq("Morgan")
+    expect(student.age).to eq(21)
+  end
+
 end
