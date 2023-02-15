098a1f6d1bfa9d83af7c679cdda012e4786946fc
Amy Spears <amspears007@gmail.com>
2023-02-15 09:19:33 -0700

    Add tests for scores in student spec file

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,11 @@
 class Student
-  attr_reader :name, :age
+  attr_reader :name, 
+              :age, 
+              :scores
 
   def initialize(info)
     @name = info[:name]
     @age = info[:age]
+    @scores = []
   end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -14,4 +14,17 @@ RSpec.describe Student do
     expect(student.age).to eq(21)
   end
 
+  it 'starts with no scores' do
+    student = Student.new({name: "Morgan", age: 21}) 
+
+    expect(student.scores).to eq([])
+  end
+
+  it 'can log scores and store them in an array' do
+    student = Student.new({name: "Morgan", age: 21}) 
+    student.log_score(89)
+    student.log_score(78)
+
+    expect(student.log_scores).to match_array([89, 78])
+  end
 end
